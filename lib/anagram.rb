893154df4afed831473e49e06fb04b66c09f0134
Kevin Downs <kevin.downs04@gmail.com>
2019-11-14 15:30:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
     sorted_word = @word.chars.sort.join
     sorted_list = poss_anagrams.map {|word| word.chars.sort.join}
     index = sorted_list.find_index{|sorted| sorted == sorted_word}
-    index ? Array.new(poss_anagrams[index]) : []
+    index ? [poss_anagrams[index]] : []
   end
 
 end
